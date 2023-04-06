5d15cc1206e1d6e2b0dccaa73324be05345114a2
JeevanNaikOp <naikjeevan97@gmail.com>
2023-04-06 14:48:56 +0530

    removed unwanted comments

--- main.py
+++ main.py
@@ -24,17 +24,13 @@ class HomePage(tornado.web.RequestHandler):
 
 class Login(tornado.web.RequestHandler):
     def post(self):
-        #base_url = 'https://api.eu-gb.apiconnect.appdomain.cloud/m1ganeshtcscom1543928228162-dev/sb/payments/custReg?acctId='
-        # 100000001001 is the only working answer
-        #headers = {'Content-Type': 'application/json'}
+
         print("inside login")
         username = str(self.get_body_argument("uname"))
         print(username)
         pwd = str(self.get_body_argument("pass"))
         print(pwd)
-        #end_url= base_url+str(self.get_body_argument("accnt"))
-        #req = requests.get(end_url, headers=headers, auth=('701e3938-c7c7-4568-9e3b-d474bfb39700', ''), verify=False)
-        #json_out = req.json()
+
         print("json")
         if username =="admin" and pwd == "adminpass":
             print("success")
@@ -42,8 +38,6 @@ class Login(tornado.web.RequestHandler):
         else:
             print("no")
             self.render("static/trial.html")
-        #print(json_out)
-        #self.render("static/genericresp.html",msg=json_out['CSRGRES']['CSRGRES']['MESSAGES'],cname=json_out['CSRGRES']['CSRGRES']['CUSTOMER_NAME'],cid=json_out['CSRGRES']['CSRGRES']['CUSTOMER_ID'],date=json_out['CSRGRES']['CSRGRES']['SYS_DATE'],time=json_out['CSRGRES']['CSRGRES']['SYS_TIME'],bloc="regreq")
 
 
 
@@ -68,11 +62,9 @@ class predictScore(tornado.web.RequestHandler):
         token = json.loads(response.text)['token']
 
         base_url = 'http://192.86.32.113:5001/iml/v2/scoring/online/86118501-fc26-4a19-8451-e54b95acae8b'
-        #base_url = 'https://gateway.aipc1.cp4i-b2e73aa4eddf9dc566faa4f42ccdd306-0001.us-east.containers.appdomain.cloud/sachinsorg/sandbox/payments/pymntRev?acctId='
-        #base_url = 'https://api.eu-gb.apiconnect.appdomain.cloud/m1ganeshtcscom1543928228162-dev/sb/payments/pymntRev?acctId='
-        # 100000001001 is the only working answer
+
         header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + token}
-        #end_url= base_url+str(self.get_body_argument("accnt"))+"&transId="+str(self.get_body_argument("trans"))+"&revAmt="+str(self.get_body_argument("debit_amt"))
+        
         amt=str(self.get_body_argument("amt"))
         cnt_children=str(self.get_body_argument("cnt_children"))
         gender=str(self.get_body_argument("gender"))
@@ -83,8 +75,7 @@ class predictScore(tornado.web.RequestHandler):
         postgraduate=str(self.get_body_argument("postgraduate"))
         income_type=str(self.get_body_argument("income_type"))
         occupation_type=str(self.get_body_argument("occupation_type"))
-        #end_url= base_url+amt+"&user1="+cnt_children+"&amount="+gender+"&merchantxstate="+birth+"&usexchip="+ext_source_1+"&errorsx="+ext_source_2+"&mcc="+graduate+"&merchantxcity="+postgraduate+"&card="+income_type+"&Occupational"+occupaton_type
-        #req = requests.get(end_url, headers=headers, auth=('ibmuser', 'ibmuser'), verify=False)
+
         payload_scoring = [{"EXT_SOURCE_2":ext_source_1,"EXT_SOURCE_3":ext_source_2,"DAYS_BIRTH":birth,"CODE_GENDER_M":gender,"NAME_EDUCATION_TYPE_Higher education":graduate,"NAME_EDUCATION_TYPE_Secondary / secondary special":postgraduate,"NAME_INCOME_TYPE_Working":income_type,"AMT_CREDIT":amt,"CNT_CHILDREN":cnt_children,"OCCUPATION_TYPE_Sales staff":occupation_type}]
         print(payload_scoring)
         response_scoring = requests.post('http://192.86.32.113:5001/iml/v2/scoring/online/86118501-fc26-4a19-8451-e54b95acae8b', json=payload_scoring, headers=header,verify=False)
@@ -97,7 +88,7 @@ class predictScore(tornado.web.RequestHandler):
         #print(jsonstruct)
         jsonstruct=json.dumps(jsonstruct)
         json_load=json.loads(jsonstruct)
-        #print(json_load["MODELOUT"]["MODELOUP"]["PROBABILITYX1X"])
+
         print("df")
         print(json_load)
         val1=json_load[0]['probability'][0]
@@ -108,18 +99,12 @@ class predictScore(tornado.web.RequestHandler):
         #print (val1)
         val1=round((val1*100),2)
         val2=round((val2*100),2)
-        #print(val1)
-        #val1=round(val1,2)
-        #print(val1)
-        #percent1=val1
-        #percent2=int(json_load['MODELOUT']['MODELOUP']['PROBABILITYX0X'])*100
+
         print(val1,val2)
         labels= ['Risk for transfer', 'Non-risk for transfer']
         colors=['#14213d','#e63946']
         sizes= [val1,val2]
-        #plt.pie(sizes,labels=labels, colors=colors, startangle=90, autopct='%1.1f%%')
-        #plt.axis('equal')
-        #plt.show()
+
         if json_load[0]['prediction']:
             outVal = 'No'
         else:
